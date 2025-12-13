e6cbd9096658408d509f862d13348a6ca3b59713
Mosiah Nthengu <mosiah.nthengu@gmail.com>
2025-12-13 16:01:30 +0200

    Update files with latest changes

--- afribicon technology/final/scripts/theme.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.getElementById("theme-toggle");
-
-toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-
-    toggleBtn.textContent =
-        document.body.classList.contains("dark-mode") ? "☀️" : "🌙";
-});
